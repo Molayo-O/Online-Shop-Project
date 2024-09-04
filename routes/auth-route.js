03aef9c800b6d6db6a7a5387bed423448503f580
Molayo Ogunfowora <molayoogunfowora@trentu.ca>
2024-09-04 13:10:27 -0400

    Add logout button dynamically

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -2,13 +2,20 @@
 import express from "express";
 
 //include controller file
-import { getSignup, signup, getLogin } from "../controllers/auth-controller.js";
+import {
+  getSignup,
+  signup,
+  getLogin,
+  login,
+  logout,
+} from "../controllers/auth-controller.js";
 
 const router = express.Router();
 
 router.get("/signup", getSignup);
 router.post("/signup", signup);
 router.get("/login", getLogin);
-
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
