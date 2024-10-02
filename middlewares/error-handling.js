00c4190ae918a623d169485cfeea2325f98b6480
Molayo Ogunfowora <molayoogunfowora@trentu.ca>
2024-10-01 23:36:58 -0400

    Add more error views

--- middlewares/error-handling.js
+++ middlewares/error-handling.js
@@ -1,5 +1,9 @@
 //this function handles any errors generated from other middlewares/route handlers
 export function handleErrors(error, req, res, next) {
   console.log(error);
+
+  if (error.code == 404) {
+    return res.status(404).render("shared-views/404-error");
+  }
   res.status(500).render("shared-views/500-error");
 }
--- routes/base-route.js
+++ routes/base-route.js
@@ -7,4 +7,12 @@ router.get("/", function (req, res) {
   res.redirect("/catalogue");
 });
 
+router.get("/401", function (req, res) {
+  res.render("shared-views/401-error");
+});
+
+router.get("/403", function (req, res) {
+  res.render("shared-views/403-error");
+});
+
 export default router;
