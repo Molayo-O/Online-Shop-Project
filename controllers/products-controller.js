d97c1678c6e5b1fd61e0e43407d1055bc9c95bce
Molayo Ogunfowora <molayoogunfowora@trentu.ca>
2024-10-03 09:53:53 -0400

    Render product-detail view

--- controllers/products-controller.js
+++ controllers/products-controller.js
@@ -1,10 +1,20 @@
 import { Product } from "../models/product-model.js";
 
-export async function getAllProducts(req, res) {
+export async function getAllProducts(req, res, next) {
   //retrieve all products
   try {
     const products = await Product.retrieveAllProducts();
     res.render("customers/products/catalogue", { products: products });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function getSingleProduct(req, res, next) {
+  //retrieve single product
+  try {
+    const product = await Product.findProductId(req.params.id);
+    res.render("customers/products/product-detail", { product: product });
   } catch (error) {
     next(error);
   }
--- routes/products-route.js
+++ routes/products-route.js
@@ -1,8 +1,10 @@
 //Handling product routes here
 import express from "express";
-import { getAllProducts } from "../controllers/products-controller.js";
+import { getAllProducts, getSingleProduct } from "../controllers/products-controller.js";
 const router = express.Router();
 
 router.get("/catalogue", getAllProducts);
+router.get("/products/:id", getSingleProduct);
+
 
 export default router;
