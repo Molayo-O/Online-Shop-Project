96c23d5b31ebd161137b1da3b61ba3a7df89cd67
Molayo Ogunfowora <molayoogunfowora@trentu.ca>
2024-11-20 12:44:47 -0500

    Add message on successful/failed payments

--- routes/order-routes.js
+++ routes/order-routes.js
@@ -2,11 +2,13 @@
 import express from "express";
 
 //include controller file
-import { submitOrder, getOrders } from "../controllers/orders-controller.js";
+import { submitOrder, getOrders, getSuccess, getFailure } from "../controllers/orders-controller.js";
 
 const router = express.Router();
 
 router.get('/', getOrders); //same as /orders/
+router.get('/success', getSuccess);
+router.get('/failure', getFailure);
 router.post('/', submitOrder); //same as /orders/
 
 
